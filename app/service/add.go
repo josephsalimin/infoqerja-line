3102c8a7550e812dd7550096d965fd4328436cbe
william.ong@xtremax.com <williamong1400@gmail.com>
2020-06-07 15:24:57 +0700

    test date picker

--- app/service/add.go
+++ app/service/add.go
@@ -1,7 +1,6 @@
 package service
 
 import (
-	"infoqerja-line/app/constant"
 	iql "infoqerja-line/app/line"
 
 	"github.com/line/line-bot-sdk-go/linebot"
@@ -12,6 +11,14 @@ type IncomingAdd struct{}
 
 // Reply : Method service for IncomingHelp instance
 func (handler *IncomingAdd) Reply(bot iql.BotClient, token string) error {
-	_, err := bot.ReplyMessage(token, linebot.NewTextMessage(constant.AddMessage)).Do()
+
+	template := linebot.NewButtonsTemplate(
+		"", "", "Select date / time !",
+		linebot.NewDatetimePickerAction("date", "DATE", "date", "", "", ""),
+		linebot.NewDatetimePickerAction("time", "TIME", "time", "", "", ""),
+		linebot.NewDatetimePickerAction("datetime", "DATETIME", "datetime", "", "", ""),
+	)
+
+	_, err := bot.ReplyMessage(token, linebot.NewTemplateMessage("Date time picker alt text", template)).Do()
 	return err
 }
